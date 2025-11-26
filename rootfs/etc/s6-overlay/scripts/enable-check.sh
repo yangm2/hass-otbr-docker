c884ff9e799216debcf19e21e80343d47af1dbea
yangm2 <yangm2@users.noreply.github.com>
2025-11-26 13:49:35 -0800

    qualify NETWORK_DEVICE

--- rootfs/etc/s6-overlay/scripts/enable-check.sh
+++ rootfs/etc/s6-overlay/scripts/enable-check.sh
@@ -17,8 +17,9 @@
 # ==============================================================================
 
 # if bashio::config.has_value 'network_device'; then
+if [ -n "$NETWORK_DEVICE" ]
     touch /etc/s6-overlay/s6-rc.d/user/contents.d/socat-otbr-tcp
     touch /etc/s6-overlay/s6-rc.d/otbr-agent/dependencies.d/socat-otbr-tcp
     # bashio::log.info "Enabled socat-otbr-tcp."
     echo "Enabled socat-otbr-tcp."
-# fi
+fi
